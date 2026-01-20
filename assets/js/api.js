211bcd0f5a9f7d4e04c250392ad27dfb0622d8d8
wallacyfalcao <wallacyfalcaodev@gmail.com>
2026-01-20 14:57:04 -0300

    incluimos totdas as iformações

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/wallacyfalcao/Portfolio-Wallacy/refs/heads/main/data/profile.json?token=GHSAT0AAAAAADRTZSYUFZC7Y5CISK6HLIWM2LPXTMA';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
