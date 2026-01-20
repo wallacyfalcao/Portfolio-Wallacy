9ae0ca9d3f405bbac779a734d92660431ead5f2a
wallacyfalcao <wallacyfalcaodev@gmail.com>
2026-01-20 15:16:51 -0300

    corrigindo as imagens das skills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/wallacyfalcao/Portfolio-Wallacy/refs/heads/main/data/profile.json?token=GHSAT0AAAAAADRTZSYUFZC7Y5CISK6HLIWM2LPXTMA';
+    const url = 'https://raw.githubusercontent.com/wallacyfalcao/Portfolio-Wallacy/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
